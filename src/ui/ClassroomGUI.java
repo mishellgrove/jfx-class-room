2a141b2bc2d5d77a969d2c53d672a087ecd1b686
mishellgrove <70043762+mishellgrove@users.noreply.github.com>
2021-02-13 08:47:38 -0500

    Update Register panel

--- src/ui/ClassroomGUI.java
+++ src/ui/ClassroomGUI.java
@@ -3,23 +3,79 @@ package ui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class ClassroomGUI {
 
-    @FXML
-    private TextField username;
+	@FXML
+	private TextField username;
 
-    @FXML
-    private TextField password;
+	@FXML
+	private TextField password;
 
-    @FXML
-    void login(ActionEvent event) {
+	@FXML
+	void login(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void singup(ActionEvent event) {
+	@FXML
+	void singup(ActionEvent event) {
 
-    }
+	}
+
+	@FXML
+	private TextField username;
+
+	@FXML
+	private TextField password;
+
+	@FXML
+	private RadioButton male;
+
+	@FXML
+	private RadioButton female;
+
+	@FXML
+	private RadioButton other;
+
+	@FXML
+	private ToggleGroup gender;
+
+	@FXML
+	private DatePicker birthday;
+
+	@FXML
+	private ChoiceBox<?> favorite;
+
+	@FXML
+	private CheckBox software;
+
+	@FXML
+	private CheckBox telematic;
+
+	@FXML
+	private CheckBox industrial;
+
+	@FXML
+	void browseButton(ActionEvent event) {
+
+	}
+
+	@FXML
+	void createAccountButton(ActionEvent event) {
+
+	}
+
+	@FXML
+	void singinButton(ActionEvent event) {
+
+	}
 
 }
+
