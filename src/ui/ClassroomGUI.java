e9a8e3423a989c2c5ffba7e32fa3490ca258341e
mishellgrove <70043762+mishellgrove@users.noreply.github.com>
2021-02-13 08:23:31 -0500

    Panels update

--- src/ui/ClassroomGUI.java
+++ src/ui/ClassroomGUI.java
@@ -1,5 +1,25 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class ClassroomGUI {
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+    @FXML
+    void singup(ActionEvent event) {
+
+    }
 
 }
